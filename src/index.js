9544c255a968547ef9ffd314e5334a4472b1f361
wsl_Dyann <wiltestrun458@gmail.com>
2022-01-16 14:39:17 +0800

    Add displaying of fetched data

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // ! Exposed API
 const WEATHER_API_KEY = `b8e5a03721491c54fbac09f715654079`;
 // TODO Add function that can take location and return weather data. CLG for now
-const fetchWeather = async (api, location = "Manila") => {
+const fetchWeather = async (api, location = "sadfdc") => {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api}`,
@@ -11,34 +11,55 @@ const fetchWeather = async (api, location = "Manila") => {
         mode: "cors",
       }
     );
-    const data = await response.json();
-    return data;
+    return response.json();
   } catch (e) {
-    console.log("I ran!");
-    throw new Error("Error", e);
+    console.log(e);
+    throw new Error(e);
   }
 };
 
 // TODO Add function that process that data (parse the json file)
-async function getWeather() {
-  try {
-    const data = await fetchWeather(WEATHER_API_KEY);
-    // ? Pick the relevant values from the api
-    console.log(data);
-    const {
-      weather: [{ main, description, icon }],
-      name,
-      sys: { country, sunrise, sunset },
-    } = data;
-    return { weather: main, description, icon, name, country, sunrise, sunset };
-  } catch (e) {
-    console.log(e);
-    throw new Error("error", e);
+async function getWeather(location) {
+  const data = await fetchWeather(WEATHER_API_KEY, location);
+  if (data.cod !== 200) {
+    return null;
   }
+  // ? Pick the relevant values from the api
+  const {
+    weather: [{ main, description, icon }],
+    name,
+    sys: { country, sunrise, sunset },
+  } = data;
+  return { weather: main, description, icon, name, country, sunrise, sunset };
 }
-// getWeather().then((data) => {
-//   console.table(data);
-// });
+
 // TODO Set a simple form that take an input
+const form = document.querySelector("form");
+const weatherWrap = document.querySelector("div#weather-wrapper");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const location = form["loc-input"].value;
+  const data = await getWeather(location);
+  if (data) {
+    renderWeather(data);
+  }
+  renderError();
+});
+function renderError() {
+  weatherWrap.innerHTML = `
+    <div>No Data. Please try again </div>`;
+}
 // TODO display the information
+function renderWeather(data) {
+  weatherWrap.innerHTML = `
+        <div> ${data.weather}</div>
+        <div> ${data.description}</div>
+        <img src="http://openweathermap.org/img/wn/${data.icon}@2x.png"> 
+        <div> ${data.name}</div>
+        <div> ${data.country}</div>
+        <div> ${data.sunrise}</div>
+        <div> ${data.sunset}</div>
+    `;
+}
 // TODO Style it! (Kinda long)
