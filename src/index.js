411c7ce584fb619dc8962176f44ee833b7946f47
wsl_Dyann <wiltestrun458@gmail.com>
2022-01-16 09:52:39 +0800

    Add TODOs

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// * Weather App
+
+// TODO Add function that can take location and return weather data. CLG for now
+// TODO Add function that process that data (parse the json file)
+// TODO Set a simple form that take an input
+// TODO display the information
+// TODO Style it! (Kinda long)
